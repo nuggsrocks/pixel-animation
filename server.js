b99534a9b984b69405464dabc7dcba68ed87f524
mattfisher <nuggsrocks@yahoo.com>
2022-02-09 22:16:11 -0500

    setup ejs

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.render('index', {foo: 'bar'})
 })
 
 const port = process.env.PORT || 8080
