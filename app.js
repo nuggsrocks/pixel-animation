4fa8b3c1e8821b10e7bf3870d48116da627ec4a8
mattfisher <nuggsrocks@yahoo.com>
2022-02-09 22:26:24 -0500

    add static files

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 app.set('view engine', 'ejs')
+
+app.use(express.static(path.resolve(__dirname, './public')))
 
 app.get('/', (req, res) => {
   res.render('index', {foo: 'bar'})
