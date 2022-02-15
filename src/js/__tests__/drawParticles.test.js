3f9a2f04b9f9d57980381d63cfb6aecfba47a6d7
mattfisher <nuggsrocks@yahoo.com>
2022-02-14 23:52:06 -0500

    test particles are drawn with correct color

--- src/js/__tests__/drawParticles.test.js
+++ src/js/__tests__/drawParticles.test.js
@@ -1,18 +1,22 @@
 const drawParticles = require('../drawParticles')
+const mockImageData = require('../../../__mocks__/mockImageData')
 
 
 describe('drawParticles', () => {
   describe('draws particles on given canvas at their respective locations', () => {
+    const imageData = mockImageData(10, 5)
+
     it.each([
-      {x: 0, y: 10, size: 5},
-      {x: 15, y: 20, size: 10},
-      {x: 100, y: 0, size: 2},
+      {x: 0, y: 1.7, size: 3},
+      {x: 1, y: 2.4, size: 1},
+      {x: 7.9, y: 0.1, size: 2},
     ])('should draw %j at point x, y with correct size', (particle) => {
       const mockCanvas = document.createElement('canvas')
       const mockCtx = mockCanvas.getContext('2d')
 
-      drawParticles(mockCtx, [particle])
+      drawParticles(mockCtx, [particle], imageData.mapped)
 
+      expect(mockCtx.__getEvents().find(obj => obj.type === 'fillStyle')).not.toBeUndefined()
       expect(mockCtx.fillRect).toHaveBeenCalledWith(particle.x, particle.y, particle.size, particle.size)
     })
   })
--- src/js/drawParticles.js
+++ src/js/drawParticles.js
@@ -1,5 +1,14 @@
-module.exports = (ctx, particles) => {
+module.exports = (ctx, particles, mappedPixels) => {
+  ctx.save()
   for (const particle of particles) {
+    const col = Math.floor(particle.x)
+    const row = Math.floor(particle.y)
+
+    if (mappedPixels[row] && mappedPixels[row][col]) {
+      ctx.fillStyle = mappedPixels[row][col].color
+    }
+
     ctx.fillRect(particle.x, particle.y, particle.size, particle.size)
   }
+  ctx.restore()
 }
